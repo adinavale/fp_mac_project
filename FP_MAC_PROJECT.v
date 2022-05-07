059aecaa5162f353aa4c3d4f43a05e87c5d0b477
Deepak Achariya <deepak.achariya@sjsu.edu>
2022-05-06 19:16:15 -0700

    fixed keypad

--- FP_MAC_PROJECT.v
+++ FP_MAC_PROJECT.v
@@ -2,6 +2,10 @@
 
 `timescale 1ns / 1ps
 
+`include "hex_code.v"
+`include "sram.v"
+`include "keypadscanner.v"
+
 module FP_MAC_PROJECT(input clk, rst,
                       input ns_button,
 							 
@@ -24,26 +28,28 @@ module FP_MAC_PROJECT(input clk, rst,
 	reg [2:0]  curr_state_sram, next_state_sram;
 	
 	reg [4:0]  arduino_out_q;
-	reg [31:0] hex_display_q;
+	reg [19:0] hex_display_q;
 	
 	wire cs_a, we_a, oe_a;
 	wire cs_b, we_b, oe_b;
 	wire key_en;
-	wire [3:0] sram_addr, key_data, sram_data_a, sram_data_b;
+	wire [3:0] sram_addr; 
+  wire [15:0] key_data, sram_data_a, sram_data_b;
 	
-	wire [31:0] hex_display_in;
+	wire [19:0] hex_display_in;
 	
 	
 	//Assign Statements
 	assign arduino_out = arduino_out_q;
 	
-	assign hex_display_in = (key_en) ? {28'b0, key_data} : hex_display_q;
+	assign hex_display_in = (key_en) ? {1'b0, key_data[15:12], 1'b0, key_data[11:8], 1'b0, key_data[7:4], 1'b0, key_data[3:0] } : hex_display_q;
 	
 	assign sram_addr = curr_state_sram;
 	assign sram_data_a = (curr_state == SRAM_A) ? key_data : 4'bz;
 	assign sram_data_b = (curr_state == SRAM_B) ? key_data : 4'bz;
 	
-	assign key_en = (curr_state == SRAM_A || curr_state == SRAM_B) ? 1'b1 : 1'b0;
+	// assign key_en = (curr_state == SRAM_A || curr_state == SRAM_B) ? 1'b1 : 1'b0;
+	assign key_en = 1'b0;
 	assign cs_a   = (curr_state == SRAM_A || curr_state == RESULT) ? 1'b1 : 1'b0;
 	assign we_a   = ~(ns_button);
 	assign we_b   = ~(ns_button);
@@ -51,17 +57,17 @@ module FP_MAC_PROJECT(input clk, rst,
 	
 	//Module Instances//
 	//HEX Display
-	hex_code hex0(.in(hex_display_in[07:00]), .out(hex0_disp));
-	hex_code hex1(.in(hex_display_in[15:08]), .out(hex1_disp));
-	hex_code hex2(.in(hex_display_in[23:16]), .out(hex2_disp));
-	hex_code hex3(.in(hex_display_in[31:24]), .out(hex3_disp));
+	hex_code hex0(.in(hex_display_in[04:00]), .out(hex0_disp));
+	hex_code hex1(.in(hex_display_in[09:05]), .out(hex1_disp));
+	hex_code hex2(.in(hex_display_in[14:10]), .out(hex2_disp));
+	hex_code hex3(.in(hex_display_in[19:15]), .out(hex3_disp));
 	
 	//SRAM
 	sram sram_a(.Cs_n(cs_a), .We_n(we_a), .Oe_n(oe_a), .Address(sram_addr), .IO(sram_data_a));
 	sram sram_b(.Cs_n(cs_b), .We_n(we_b), .Oe_n(oe_b), .Address(sram_addr), .IO(sram_data_b));
 	
 	//Keyboard
-	keyboard_read keyboard (.clk(clk), .rst(rst), .key_row(key_row), .key_col(key_col), .enable(key_en), .data_out(key_data));
+  KeyPadScanner keyboard (.Clock(clk), .reset(~key_en), .RowIn(key_row),   .ColOut(key_col),  .KeyRd(key_en),  .mem_reg(key_data));
 	
 	//FSMs
 	
@@ -71,7 +77,9 @@ module FP_MAC_PROJECT(input clk, rst,
 	  if(!rst) 
 	  begin
 	    curr_state <= RESET;
-		 curr_state_sram <= INPUT_0; 
+		  curr_state_sram <= INPUT_0;
+      next_state <= RESET;
+      next_state_sram <= INPUT_0;
 	  end
 	  else
 	  begin 
@@ -85,69 +93,124 @@ module FP_MAC_PROJECT(input clk, rst,
 	begin
 	  
 	  arduino_out_q = curr_state + curr_state_sram;
-	  hex_display_q = 32'h0;
+	  hex_display_q = 20'h0;
 	  
 	  case(curr_state)
 	    RESET :
 		   begin
-			  hex_display_q = 32'hFFFF_FFFF; // BLANK 7-SegD
-			  next_state = SRAM_A;
-			  next_state_sram = INPUT_0;
+			  hex_display_q = 20'hF_FFFF; // BLANK 7-SegD
+
+        if(ns_button == 0)
+        begin
+			    next_state = SRAM_A;
+			    next_state_sram = INPUT_0;
+        end
+
 			end
 		 SRAM_A :
 		   begin
 			  case(curr_state_sram)
 			    INPUT_0 :
 				   begin
-					  next_state = SRAM_A;
-					  next_state_sram = INPUT_1;
-					end
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_A;
+					     next_state_sram = INPUT_1;
+             end
+					
+           end
 				 INPUT_1 :
 				   begin
-					  next_state = SRAM_A;
-					  next_state_sram = INPUT_2;
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_A;
+					     next_state_sram = INPUT_2;
+             end
+
 					end 
 				 INPUT_2 :
 				   begin
-					  next_state = SRAM_A;
-					  next_state_sram = INPUT_3;
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_A;
+					     next_state_sram = INPUT_3;
+             end
+
 					end
 				 INPUT_3 :
 				   begin
-					  next_state = SRAM_A;
-					  next_state_sram = INPUT_4;
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_A;
+					     next_state_sram = INPUT_4;
+             end
+
 					end
 				 INPUT_4 :
 				   begin
-					  next_state = SRAM_A;
-					  next_state_sram = INPUT_5;
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_A;
+					     next_state_sram = INPUT_5;
+             end
+
 					end
 				 INPUT_5 :
 				   begin
-					  next_state = SRAM_A;
-					  next_state_sram = INPUT_6;
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_A;
+					     next_state_sram = INPUT_6;
+             end
+
 					end
 				 INPUT_6 :
 				   begin
-					  next_state = SRAM_A;
-					  next_state_sram = INPUT_7;
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_A;
+					     next_state_sram = INPUT_7;
+             end
+
 					end
 				 INPUT_7 :
 				   begin
-					  next_state = SRAM_B;
-					  next_state_sram = INPUT_0;
+
+             if(ns_button == 0)
+             begin
+					     next_state = SRAM_B;
+					     next_state_sram = INPUT_0;
+             end
+
 					end
 			  endcase
 			end
 		 SRAM_B :
 		   begin
-			  next_state = RESULT;
-			  next_state_sram = INPUT_0;
+
+         if(ns_button == 0)
+         begin
+			     next_state = RESULT;
+			     next_state_sram = INPUT_0;
+         end
+
 			end
 		 RESULT : 
 		   begin
-			  next_state = RESULT;
-			  next_state_sram = INPUT_0;
+
+         if(ns_button == 0)
+         begin
+			     next_state = RESULT;
+			     next_state_sram = INPUT_0;
+         end
+
 			end
 	  endcase
 	end
---
+++ top.sv
@@ -0,0 +1,66 @@
+`include "FP_MAC_PROJECT.v"
+
+`timescale 1ns / 1ps
+
+module top();
+
+  logic clk, rst;
+  logic ns_button;
+  logic [3:0] key_row, key_col;
+  logic [4:0] arduino_out;
+  logic [7:0] hex0_disp, hex1_disp, hex2_disp, hex3_disp;
+
+
+  FP_MAC_PROJECT dut(.clk(clk), .rst(rst), .ns_button(ns_button), .key_row(key_row), .key_col(key_col),
+      .arduino_out(arduino_out), .hex0_disp(hex0_disp), .hex1_disp(hex1_disp), .hex2_disp(hex2_disp), .hex3_disp(hex3_disp));
+
+  initial
+  begin
+    #50ns;
+    rst = 1;
+
+    repeat(5) @ (posedge clk);
+    ns_button = 0;
+    @(negedge clk);
+    ns_button = 1;
+
+
+    repeat(1000000) @ (posedge clk);
+    ns_button = 0;
+    @(negedge clk);
+    ns_button = 1;
+  end
+
+  initial
+  begin
+    #2000us;
+    $finish;
+  end
+
+
+  always @ (key_col)
+  begin
+		case(key_col) 
+			4'bzzz0: key_row = 4'b1111;
+			4'bzz0z: key_row = 4'b1111;
+			4'bz0zz: key_row = 4'b1101;
+			4'b0zzz: key_row = 4'b1111;
+		endcase
+  end
+
+  initial
+  begin
+    clk = 0;
+    rst = 0;
+    key_row = 4'b1111;
+    ns_button = 1;
+    forever #(10ns) clk = ~clk;
+  end
+
+  initial
+  begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+
+endmodule
